824a819af110cbc141eef6044ce1fb9e7a929671
Conrad de Wet <58937573+InnovateAsterisk@users.noreply.github.com>
2025-04-25 13:12:51 +0200

    Version 0.0.31

--- Phone/phone.js
+++ Phone/phone.js
@@ -15,7 +15,7 @@
 
 // Global Settings
 // ===============
-const appversion = "0.3.29";
+const appversion = "0.3.31";
 const sipjsversion = "0.20.0";
 const navUserAgent = window.navigator.userAgent;  // TODO: change to Navigator.userAgentData
 const instanceID = String(Date.now());
